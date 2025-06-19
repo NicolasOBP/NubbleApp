7868f38aecbf247272a9b1e87d92745a08fabe9d
NicolasOBP <nicolasobp15@gmail.com>
2025-06-19 20:11:39 -0300

    text 2 - custom fonts

--- App.tsx
+++ App.tsx
@@ -4,7 +4,12 @@ import { Text } from './src/Components/Text/text';
 function App() {
   return (
     <SafeAreaView>
-      <Text preset="headingLarge">AAAA</Text>
+      <Text preset="headingLarge" style={{ fontFamily: 'Satoshi-Black' }}>
+        AAAA
+      </Text>
+      <Text preset="headingLarge" style={{ fontFamily: 'Satoshi-Bold' }}>
+        AAAA
+      </Text>
       <Text preset="headingSmall">AAAA</Text>
       <Text preset="headingLarge">AAAA</Text>
     </SafeAreaView>

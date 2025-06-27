ae4d5db00c4172ac36de507e5bf64b4206af3b23
NicolasOBP <nicolasobp15@gmail.com>
2025-06-26 22:06:44 -0300

    forgot password

--- src/screens/auth/ForgotPasswordScreen/ForgotPasswordScreen.tsx
+++ src/screens/auth/ForgotPasswordScreen/ForgotPasswordScreen.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import { Screen } from '../../../components/Screen/Screen';
 import { Text } from '../../../components/Text/Text';
-import { TextInput } from '../../../components/TextInput/TextInput';
 import { Button } from '../../../components/Button/Button';
 import { useResetNavigationSuccess } from '../../../hooks/useResetNavigationSuccess';
+import {
+  forgotPasswordSchema,
+  ForgotPasswordSchema,
+} from './forgotPasswordSchema';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { FormTextInput } from '../../../components/Form/FormTextInput';
 
 export function ForgotPasswordScreen() {
   const { reset } = useResetNavigationSuccess();
+  const { control, formState, handleSubmit } = useForm<ForgotPasswordSchema>({
+    resolver: zodResolver(forgotPasswordSchema),
+    defaultValues: {
+      email: '',
+    },
+    mode: 'onChange',
+  });
 
-  function submitForm() {
+  function submitForm(formValues: ForgotPasswordSchema) {
     // TODO: implementar
+
+    console.log(formValues);
 
     reset({
       description:
@@ -17,13 +32,6 @@ export function ForgotPasswordScreen() {
       title: 'Enviamos as instruções para seu e-mail',
       icon: { name: 'messageRound', color: 'primary' },
     });
-
-    // navigation.navigate('SuccessScreen', {
-    //   description:
-    //     'Clique no link enviado no seu e-mail para recuperar sua senha',
-    //   title: 'Enviamos as instruções para seu e-mail',
-    //   icon: { name: 'messageRound', color: 'primary' },
-    // });
   }
 
   return (
@@ -35,13 +43,19 @@ export function ForgotPasswordScreen() {
         Digite seu e-mail e enviaremos as instruções para redefinição de senha
       </Text>
 
-      <TextInput
+      <FormTextInput
+        control={control}
+        name="email"
         label="E-mail"
         placeholder="Digite seu e-mail"
         boxProps={{ mb: 's24' }}
       />
 
-      <Button title="Recuperar senha" onPress={submitForm} />
+      <Button
+        disabled={!formState.isValid}
+        title="Recuperar senha"
+        onPress={handleSubmit(submitForm)}
+      />
     </Screen>
   );
 }

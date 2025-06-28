1e37d7148e549b5c4d58180d432dbfa02e0c7749
NicolasOBP <nicolasobp15@gmail.com>
2025-06-28 10:13:00 -0300

    fix require cycle

--- src/Components/TextInput/textInput.tsx
+++ src/Components/TextInput/textInput.tsx
@@ -5,8 +5,9 @@ import {
   TextInputProps as RNTextInputProps,
   TextStyle,
 } from 'react-native';
-import { $fontFamily, $fontSizes, Box, BoxProps, Text } from '@components';
 import { useAppTheme } from '@hooks';
+import { Box, BoxProps } from '../Box/Box';
+import { $fontFamily, $fontSizes, Text } from '../Text/Text';
 
 export interface TextInputProps extends RNTextInputProps {
   label: string;

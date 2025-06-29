d502752c2e90483b7fa4788171ea63f2f31b6cb8
NicolasOBP <nicolasobp15@gmail.com>
2025-06-29 19:52:44 -0300

    auth flow

---
+++ src/screens/app/HomeScreen/HomeScreen.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+
+import { Button, Screen, Text } from '@components';
+import { AppStackParamList } from '@routes';
+
+type ScreenProps = NativeStackScreenProps<AppStackParamList, 'HomeScreen'>;
+
+export function HomeScreen({ navigation }: ScreenProps) {
+  return (
+    <Screen>
+      <Text preset="headingLarge">Home Screen</Text>
+      <Button
+        onPress={() => navigation.navigate('SettingsScreen')}
+        title="Settings"
+      />
+    </Screen>
+  );
+}

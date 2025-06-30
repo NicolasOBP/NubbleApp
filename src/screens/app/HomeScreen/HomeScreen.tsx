0f0786422086bdf18ad2ee084ba02ffa54edaf1a
NicolasOBP <nicolasobp15@gmail.com>
2025-06-30 19:48:23 -0300

    post item draft

--- src/screens/app/HomeScreen/HomeScreen.tsx
+++ src/screens/app/HomeScreen/HomeScreen.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import { Dimensions, FlatList, Image, ListRenderItemInfo } from 'react-native';
 
 import { Post, postService } from '@domain';
 
-import { Screen, Text } from '@components';
+import { Box, Screen, Text } from '@components';
 import { AppTabScreenProps } from '@routes';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -13,11 +14,34 @@ export function HomeScreen({ navigation }: AppTabScreenProps<'HomeScreen'>) {
     postService.getList().then(list => setPostList(list));
   }, []);
 
+  function renderItem({ item }: ListRenderItemInfo<Post>) {
+    return (
+      <Box pb="s8">
+        <Box flexDirection="row">
+          <Image
+            source={{ uri: item.author.profileURL }}
+            width={32}
+            height={32}
+          />
+          <Text>{item.author.userName}</Text>
+        </Box>
+
+        <Image
+          source={{ uri: item.imageURL }}
+          width={Dimensions.get('screen').width}
+          height={300}
+        />
+      </Box>
+    );
+  }
+
   return (
     <Screen>
-      {postList.map(post => (
-        <Text key={post.id}>{post.text}</Text>
-      ))}
+      <FlatList
+        data={postList}
+        keyExtractor={item => item.id}
+        renderItem={renderItem}
+      />
     </Screen>
   );
 }

a5ad4281b59d6f5a84611f84c1887312e4e3f618
NicolasOBP <nicolasobp15@gmail.com>
2025-06-30 20:04:52 -0300

    PosItem 1 - header and image

---
+++ src/Components/PostItem/PostItem.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { Post } from '@domain';
+
+import { Box } from '@components';
+
+import { PostHeader } from './components/PostHeader';
+import { PostImage } from './components/PostImage';
+
+interface Props {
+  post: Post;
+}
+
+export function PostItem({ post }: Props) {
+  return (
+    <Box pb="s8">
+      <PostHeader author={post.author} />
+
+      <PostImage imageURL={post.imageURL} />
+    </Box>
+  );
+}

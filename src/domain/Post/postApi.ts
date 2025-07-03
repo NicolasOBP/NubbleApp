ec4812922237c14b79a95abc1565ff1accd175c4
NicolasOBP <nicolasobp15@gmail.com>
2025-07-03 20:04:57 -0300

    integração fetch API

--- src/domain/Post/postApi.ts
+++ src/domain/Post/postApi.ts
@@ -2,7 +2,18 @@ import { postListMock } from './postListMock';
 import { Post } from './types';
 
 async function getList(): Promise<Post[]> {
-  await new Promise(resolve => setTimeout(() => resolve(''), 1000));
+  let response = await fetch('http://localhost:3333/user/post', {
+    method: 'GET',
+    headers: {
+      Authorization:
+        'Bearer NQ.dX0pQjpCn_vr9E97FKA7hbwbZXnmhrVOpVwkZuJwJZYlajl7TC-rQwZsNIng',
+    },
+  });
+
+  let data = await response.json();
+  console.log(data);
+
+  // await new Promise(resolve => setTimeout(() => resolve(''), 1000));
 
   return postListMock;
 }

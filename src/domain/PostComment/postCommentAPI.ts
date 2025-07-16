ab467f19d2563cc77fd90b525f330cccb4cdd394
NicolasOBP <nicolasobp15@gmail.com>
2025-07-16 20:06:48 -0300

    post comment: api, service and adapter

---
+++ src/domain/PostComment/postCommentAPI.ts
@@ -0,0 +1,19 @@
+import { API, PageAPI, PageParams } from '@api';
+
+import { PostCommentAPI } from './postCommentTypes';
+
+async function getList(
+  post_id: number,
+  pageParams?: PageParams,
+): Promise<PageAPI<PostCommentAPI>> {
+  const response = await API.get<PageAPI<PostCommentAPI>>(
+    '/user/post_comment',
+    { params: { post_id, ...pageParams } },
+  );
+
+  return response.data;
+}
+
+export const postCommentApi = {
+  getList,
+};
---
+++ src/domain/PostComment/postCommentService.ts
@@ -0,0 +1,26 @@
+import { apiAdapter } from '@api';
+import { Page } from '@types';
+
+import { postCommentAdapter } from './postCommentAdapter';
+import { postCommentApi } from './postCommentAPI';
+import { PostComment } from './postCommentTypes';
+
+const PER_PAGE = 10;
+async function getList(
+  postID: number,
+  page: number,
+): Promise<Page<PostComment>> {
+  const postCommentPageAPI = await postCommentApi.getList(postID, {
+    page,
+    per_page: PER_PAGE,
+  });
+
+  return {
+    data: postCommentPageAPI.data.map(postCommentAdapter.toPostComment),
+    meta: apiAdapter.toMetaDataPage(postCommentPageAPI.meta),
+  };
+}
+
+export const postService = {
+  getList,
+};

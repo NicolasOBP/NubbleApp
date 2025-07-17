5400c76e404db2599913ff4769234e01d9b08179
NicolasOBP <nicolasobp15@gmail.com>
2025-07-17 19:38:36 -0300

    relative date with date-fns

--- babel.config.js
+++ babel.config.js
@@ -17,6 +17,7 @@ module.exports = {
           '@brand': './src/brand',
           '@api': './src/api',
           '@types': './src/types',
+          '@utils': './src/utils',
         },
       },
     ],
--- src/domain/PostComment/postCommentAdapter.ts
+++ src/domain/PostComment/postCommentAdapter.ts
@@ -1,3 +1,5 @@
+import { dateUtils } from '@utils';
+
 import { PostComment, PostCommentAPI } from './postCommentTypes';
 
 export function toPostComment(postCommentAPI: PostCommentAPI): PostComment {
@@ -5,6 +7,7 @@ export function toPostComment(postCommentAPI: PostCommentAPI): PostComment {
     id: postCommentAPI.id,
     message: postCommentAPI.message,
     createdAt: postCommentAPI.created_at,
+    createdAtRelative: dateUtils.formatRelative(postCommentAPI.created_at),
     author: {
       id: postCommentAPI.user.id,
       profileURL: postCommentAPI.user.profile_url,

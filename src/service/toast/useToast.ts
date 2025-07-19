14ba7604b0aa1c70d8f250fdefb9b0168923d384
NicolasOBP <nicolasobp15@gmail.com>
2025-07-18 21:38:48 -0300

    toast service (zustand)

--- src/service/toast/useToast.ts
+++ src/service/toast/useToast.ts
@@ -1,6 +1,17 @@
 import { ToastService } from './toastTypes';
-import { useToastContext } from './useToastContext';
+// import { useToastContext } from './useToastContext';
+import { useToastServiceZustand, useToastZustand } from './useToastZustand';
 
-export function useToast(): ToastService {
-  return useToastContext();
+export function useToast(): ToastService['toast'] {
+  // const { toast } = useToastContext();
+  const toast = useToastZustand();
+
+  return toast;
+}
+
+export function useToastService(): Omit<ToastService, 'toast'> {
+  // const { showToast, hideToast } = useToastContext();
+  const { hideToast, showToast } = useToastServiceZustand();
+
+  return { showToast, hideToast };
 }

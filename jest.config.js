1ac2d3b4d76de48c8ba144a37341ff522e4fe73f
NicolasOBP <nicolasobp15@gmail.com>
2025-08-15 19:56:02 -0300

    install React Native Testing Library lib

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: 'react-native',
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   coveragePathIgnorePatterns: ['/node_modules/', 'index'],
   collectCoverageFrom: ['src/{components,utils}/**/*.{js,jsx,ts,tsx}'],
 };

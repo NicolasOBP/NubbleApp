cf01e55c416dbbb37b094eb9b2ed7597af3a228f
NicolasOBP <nicolasobp15@gmail.com>
2025-08-16 19:33:28 -0300

    test ui props

--- src/Components/Button/__tests__/Button.test.tsx
+++ src/Components/Button/__tests__/Button.test.tsx
@@ -1,19 +1,47 @@
 import React from 'react';
+import { StyleSheet } from 'react-native';
 
 import { fireEvent, render, screen } from 'test-utils';
 
-import { Button } from '../Button';
+import { theme } from '@theme';
+
+import { Button, ButtonProps } from '../Button';
+
+function renderComponent(props?: Partial<ButtonProps>) {
+  render(<Button title="Button Title" {...props} />);
+
+  const titleElement = screen.getByText('button title', { exact: false });
+
+  return {
+    titleElement,
+  };
+}
 
 describe('<Button />', () => {
-  it('calls the onPress function when is pressed', () => {
+  it('calls the onPress function when it is pressed', () => {
     const mockedOnPress = jest.fn();
-
-    render(<Button title="Button Title" onPress={mockedOnPress} />);
-
-    const titleElement = screen.getByText('button title', { exact: false });
+    const { titleElement } = renderComponent({ onPress: mockedOnPress });
 
     fireEvent.press(titleElement);
-
     expect(mockedOnPress).toHaveBeenCalled();
+  });
+
+  it('does not call onPress function when it is disabled and pressed', () => {
+    const mockedOnPress = jest.fn();
+    const { titleElement } = renderComponent({
+      onPress: mockedOnPress,
+      disabled: true,
+    });
+
+    fireEvent.press(titleElement);
+    expect(mockedOnPress).not.toHaveBeenCalled();
+  });
+
+  test('the title should be gray if button is disabled', () => {
+    const { titleElement } = renderComponent({ disabled: true });
+
+    const titleStyle = StyleSheet.flatten(titleElement.props.style);
+
+    expect(titleStyle.color).toEqual(theme.colors.gray2);
   });
 });

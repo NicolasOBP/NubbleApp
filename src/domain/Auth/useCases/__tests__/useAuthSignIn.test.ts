c25b2d8d63c9fdc83093354f1eb8649cf1cc6ace
NicolasOBP <nicolasobp15@gmail.com>
2025-08-22 19:30:56 -0300

    test saveCredentials

--- src/domain/Auth/useCases/__tests__/useAuthSignIn.test.ts
+++ src/domain/Auth/useCases/__tests__/useAuthSignIn.test.ts
@@ -5,6 +5,19 @@ import { useAuthSignIn } from '../useAuthSignIn';
 
 import { mockedAuthCredentials } from './mockedData/mocks';
 
+const mockedSaveCredentials = jest.fn();
+
+jest.mock('@service', () => {
+  const originalModule = jest.requireActual('@service');
+
+  return {
+    ...originalModule,
+    useAuthCredentials: () => ({
+      saveCredentials: mockedSaveCredentials,
+    }),
+  };
+});
+
 describe('useAuthSignIn', () => {
   it('saves credentials if the sign-in successfully', async () => {
     jest
@@ -18,6 +31,8 @@ describe('useAuthSignIn', () => {
     result.current.signIn({ email: '123@123.com', password: '123' });
 
     await waitFor(() => expect(result.current.isSuccess).toBe(true));
+
+    expect(mockedSaveCredentials).toHaveBeenCalledWith(mockedAuthCredentials);
   });
   it('call the onError function with a message if sign-in fails', () => {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars

617b6be229a0e958cab72fc5f48e619c5cf3df5c
NicolasOBP <nicolasobp15@gmail.com>
2025-08-26 17:20:23 -0300

    install msw

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'react-native',
   coveragePathIgnorePatterns: ['/node_modules/', 'index'],
   collectCoverageFrom: [
-    'src/{components,utils,hooks,domain}/**/*.{js,jsx,ts,tsx}',
+    'src/{components,utils,hooks,domain,screens}/**/*.{js,jsx,ts,tsx}',
   ],
   transformIgnorePatterns: [
     'node_modules/(?!(@react-native|react-native|@react-navigation|react-native-safe-area-context)/)',

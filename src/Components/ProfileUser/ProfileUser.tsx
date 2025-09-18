a164717c37196da1b116857e56ca11b5575c489e
NicolasOBP <nicolasobp15@gmail.com>
2025-09-18 19:55:29 -0300

    ProfileUser, refactor PostHeader

--- src/Components/ProfileUser/ProfileUser.tsx
+++ src/Components/ProfileUser/ProfileUser.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import { Pressable } from 'react-native';
 
-import { Post } from '@domain';
+import { User } from '@domain';
 import { useNavigation } from '@react-navigation/native';
 
 import { Box, Text, ProfileAvatar } from '@components';
 
-type Props = Pick<Post, 'author'>;
+type ProfileUserProps = { user: Pick<User, 'username' | 'profileUrl' | 'id'> };
 
-export function PostHeader({ author }: Props) {
+export function ProfileUser({ user }: ProfileUserProps) {
   const navigation = useNavigation();
 
   function navigateToProfileScreen() {
-    navigation.navigate('ProfileScreen', { userId: author.id });
+    navigation.navigate('ProfileScreen', { userId: user.id });
   }
 
   return (
     <Pressable onPress={navigateToProfileScreen}>
       <Box flexDirection="row" alignItems="center" mb="s16">
-        <ProfileAvatar imageURL={author.profileURL} />
+        <ProfileAvatar imageURL={user.profileUrl} />
         <Text ml="s12" preset="paragraphMedium" semibold>
-          {author.userName}
+          {user.username}
         </Text>
       </Box>
     </Pressable>

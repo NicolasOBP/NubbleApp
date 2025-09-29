6ed7e0d8cdfef3d4c3d5fc2098f6752ae65942af
NicolasOBP <nicolasobp15@gmail.com>
2025-09-29 20:18:41 -0300

    clear user list on sign out

--- src/domain/Auth/useCases/useAuthSignOut.ts
+++ src/domain/Auth/useCases/useAuthSignOut.ts
@@ -1,16 +1,20 @@
-import { useAuthCredentials } from '@service';
+import { useAuthCredentials, useSearchHistoryService } from '@service';
 import { useMutation } from '@tanstack/react-query';
 
 import { authService } from '../authService';
 
 export function useAuthSignOut() {
   const { removeCredentials } = useAuthCredentials();
+  const { clearUserList } = useSearchHistoryService();
 
   const { mutate, isPending } = useMutation<string, unknown, void>({
     mutationFn: authService.signOut,
     retry: false,
 
-    onSuccess: removeCredentials,
+    onSettled: () => {
+      removeCredentials();
+      clearUserList();
+    },
   });
 
   function signOut() {

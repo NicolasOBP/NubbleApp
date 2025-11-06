a091dd8338272ac4e35044f88433158ef4e9aff8
NicolasOBP <nicolasobp15@gmail.com>
2025-11-06 17:57:34 -0300

    usePostCreate

---
+++ src/domain/Post/useCases/usePostCreate.ts
@@ -0,0 +1,47 @@
+import { MutationOptions, QueryKeys } from '@infra';
+import { ImageForUpload, multimediaService } from '@service';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+import { postService } from '../postService';
+import { Post } from '../postTypes';
+
+export function usePostCreate(option?: MutationOptions<Post>) {
+  const queryClient = useQueryClient();
+  const { mutate, isPending, isError } = useMutation<
+    Post,
+    unknown,
+    { text: string; imageCover: ImageForUpload }
+  >({
+    mutationFn: ({ text, imageCover }) =>
+      postService.createPost(text, imageCover),
+    onSuccess: post => {
+      if (option?.onSuccess) {
+        option.onSuccess(post);
+      }
+      queryClient.invalidateQueries({ queryKey: [QueryKeys.PostList] });
+    },
+    onError: () => {
+      if (option?.onError) {
+        option.onError(option?.errorMessage || 'erro ao criar post');
+      }
+    },
+  });
+
+  function createPost({
+    description,
+    imageUri,
+  }: {
+    description: string;
+    imageUri: string;
+  }) {
+    const imageForUpload = multimediaService.prepareImageForUpload(imageUri);
+
+    mutate({ text: description, imageCover: imageForUpload });
+  }
+
+  return {
+    isLoading: isPending,
+    isError,
+    createPost,
+  };
+}

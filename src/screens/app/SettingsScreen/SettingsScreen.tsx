26f6db7ba98cbb76e929eeff1a0a9ab96bbcf2f3
NicolasOBP <nicolasobp15@gmail.com>
2025-11-08 12:55:52 -0300

    settings screen

--- src/screens/app/SettingsScreen/SettingsScreen.tsx
+++ src/screens/app/SettingsScreen/SettingsScreen.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { FlatList, ListRenderItemInfo } from 'react-native';
 
 import { useAuthSignOut } from '@domain';
 
-import { Button, Screen } from '@components';
+import { Button, Screen, Separator } from '@components';
 import { AppScreenProps } from '@routes';
+
+import { MenuItem, MenuItemProps } from './components/MenuItem';
 
 export function SettingsScreen({
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -11,9 +14,32 @@ export function SettingsScreen({
 }: AppScreenProps<'SettingsScreen'>) {
   const { signOut, isLoading } = useAuthSignOut();
 
+  const items: MenuItemProps[] = [
+    { label: 'Termo de Uso', onPress: () => {} },
+    { label: 'Política de Privacidade', onPress: () => {} },
+    { label: 'Modo Escuro', onPress: () => {} },
+  ];
+
+  function renderItem({ item }: ListRenderItemInfo<MenuItemProps>) {
+    return <MenuItem {...item} />;
+  }
+
   return (
-    <Screen canGoBack title="Configurações">
-      <Button loading={isLoading} title="Sair da conta" onPress={signOut} />
+    <Screen canGoBack title="Configurações" flex={1}>
+      <FlatList
+        data={items}
+        bounces={false}
+        renderItem={renderItem}
+        ItemSeparatorComponent={Separator}
+        ListFooterComponent={
+          <Button
+            mt="s48"
+            loading={isLoading}
+            title="Sair da conta"
+            onPress={signOut}
+          />
+        }
+      />
     </Screen>
   );
 }

fa1cd9035449cc2aa67c3f44d57950b0f0ccda73
NicolasOBP <nicolasobp15@gmail.com>
2025-11-10 21:29:15 -0300

    onboarding images

--- src/screens/onboarding/OnboardingScreen/components/OnboardingPage.tsx
+++ src/screens/onboarding/OnboardingScreen/components/OnboardingPage.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 
 import { Box } from '@components';
 
+import { ImageHeader } from './ImageHeader';
+
 export function OnboardingPage() {
   return (
     <Box flex={1}>
-      <Box flex={4} bg="error" />
+      <Box flex={4} bg="error">
+        <ImageHeader />
+      </Box>
       <Box flex={5} bg="carrotSecondary" />
       <Box flex={1} bg="success" />
     </Box>

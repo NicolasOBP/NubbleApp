0e07d01894abd8bf110e54bc4aa0d7468c4ba801
NicolasOBP <nicolasobp15@gmail.com>
2026-01-12 20:09:17 -0300

    Add errorUtils for improved error handling

--- src/domain/Auth/useCases/useAuthEditPassword.ts
+++ src/domain/Auth/useCases/useAuthEditPassword.ts
@@ -1,5 +1,6 @@
 import { MutationOptions } from '@infra';
 import { useMutation } from '@tanstack/react-query';
+import { errorUtils } from '@utils';
 
 import { authService } from '../authService';
 import { EditPasswordParams } from '../authTypes';
@@ -12,6 +13,11 @@ export function useAuthEditPassword(options?: MutationOptions<string>) {
   >({
     mutationFn: params => authService.editPassword(params),
     retry: false,
+    onError: error => {
+      if (options?.onError) {
+        options.onError(errorUtils.getErrorMessage(error));
+      }
+    },
     onSuccess: message => {
       if (options?.onSuccess) {
         options.onSuccess(message);

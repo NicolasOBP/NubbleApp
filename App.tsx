514f7b460aecc33862e5cac40b91b51450e94b8c
NicolasOBP <nicolasobp15@gmail.com>
2026-02-02 21:42:23 -0300

    test tokens FCM

--- App.tsx
+++ App.tsx
@@ -24,6 +24,9 @@ const queryClient = new QueryClient();
 initializeStorate(MMKVStorage);
 
 async function requestPermission() {
+  const token = messaging().getToken();
+  console.log(token);
+
   if (Platform.OS === 'ios') {
     const authStatus = await messaging().requestPermission();
     const enabled =
